365d76247e484951793147ac6e3ca26f1461c346
Ratmir Gaitov <gaitovrat@gmail.com>
2022-08-19 21:39:36 +0400

    Move backup files to temporary directory

--- init.el
+++ init.el
@@ -26,6 +26,21 @@
 (when (display-graphic-p)
   (load-theme 'solarized-dark t))
 
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
