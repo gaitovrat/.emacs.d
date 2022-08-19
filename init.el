f1c8ce3884d2e78691c5ea53098de59768aefc5e
Ratmir Gaitov <gaitovrat@gmail.com>
2022-08-19 21:44:14 +0400

    Add ranger support

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
 
 (install-package 'evil)
 (install-package 'solarized-theme)
+(install-package 'ranger)
 
 (require 'evil)
 (evil-mode 1)
@@ -31,22 +32,14 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-(message "Deleting old backup files...")
-(let ((week (* 60 60 24 7))
-      (current (float-time (current-time))))
-  (dolist (file (directory-files temporary-file-directory t))
-    (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
-                  week))
-      (message "%s" file)
-      (delete-file file))))
+(ranger-override-dired-mode t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(solarized-theme evil)))
+ '(package-selected-packages '(ranger solarized-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
